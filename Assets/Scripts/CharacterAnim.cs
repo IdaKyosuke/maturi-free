05be575cb33b798dbd37f40d4aded63c00754436
hmj2422030-admin <hmj2422030@stu.o-hara.ac.jp>
2025-07-02 13:40:55 +0900

    プレイヤーが入場をするようになった。

---
+++ Assets/Scripts/CharacterAnim.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+[DefaultExecutionOrder(-1)]
+public class CharacterAnim : MonoBehaviour
+{
+	private Animator m_anim;
+
+	private Vector3 m_startPos;	// 移動開始時の場所
+	private Vector3 m_goalPos;    // 移動の目標地点
+	private bool m_canMove; // 移動できるか
+
+	private float m_duration = 0;	// 経過時間計測用
+	private float m_moveTime = 0;   // 移動にかかる時間
+
+	[SerializeField] float m_diffY;	// 高さの補正（必要なら付ける）
+
+	private void Start()
+	{
+		m_anim = GetComponent<Animator>();
+		m_canMove = false;
+	}
+
+	private void Update()
+	{
+		if(m_canMove)
+		{
+			m_duration += Time.deltaTime;
+			float t = m_duration / m_moveTime;
+			if(t >= 1.0f)
+			{
+				t = 1.0f;
+				m_duration = 0;
+				m_canMove = false;
+				SetRun(false);	
+			}
+
+			transform.position = Vector3.Lerp(m_startPos, m_goalPos, t);
+		}
+	}
+
+	// ----- 共通の処理 -----
+	public void SetRun(bool isRun)
+	{
+		// 走るアニメーション
+		m_anim.SetBool("run", isRun);
+	}
+
+	public void GetHit()
+	{
+		// ダメージを受けるアニメーション
+		m_anim.SetTrigger("getHit");
+	}
+
+	public void Death()
+	{
+		// 死亡アニメーション
+		m_anim.SetTrigger("death");
+	}
+
+	// ----- 攻撃アニメーション -----
+	public void Attack(int attackNum)
+	{
+		m_anim.SetTrigger("attack" + attackNum);
+	}
+
+	// 現在の座標が戦う場所かどうか
+	public bool IsBattlePos()
+	{
+		return transform.position == m_goalPos;
+	}
+
+	// 移動する
+	public void Move(Transform startPos, Transform goalPos, float moveTime)
+	{
+		// スタート地点とゴール地点を決める
+		m_startPos = startPos.position;
+		m_goalPos = goalPos.position;
+
+		m_startPos.y += m_diffY;
+		m_goalPos.y += m_diffY;
+
+		// 移動時間を設定する
+		m_moveTime = moveTime;
+
+		// 移動フラグを立てる
+		m_canMove = true;
+
+		SetRun(true);
+	}
+}
---
+++ Assets/Scripts/StartBattle.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class StartBattle : MonoBehaviour
+{
+	[SerializeField] GameObject[] m_players;    // 味方
+	[SerializeField] float m_startInTime = 1.0f;    // 戦闘開始時の入場にかかる時間
+	[SerializeField] Transform m_startPos;  // 入場開始時の場所
+	[SerializeField] Transform[] m_battlePos;	// 戦闘時の場所
+
+	private bool m_isStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		for(int i = 0; i < m_players.Length; i++)
+		{
+			m_players[i].GetComponent<CharacterAnim>().Move(m_startPos, m_battlePos[i], m_startInTime);
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!m_isStart)
+		{
+			int count = 0;
+			for(int i = 0; i < m_players.Length; i++)
+			{
+				// 全員が場所につくまで待つ
+				if(m_players[i].GetComponent<CharacterAnim>().IsBattlePos())
+				{
+					count++;
+				}
+			}
+
+			if(count == m_players.Length)
+			{
+				// 到着したら戦闘開始
+				m_isStart = true;
+			}
+		}
+    }
+}
